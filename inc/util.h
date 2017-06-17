26e0a4ae224ede1e04343fe08108b0b25d9ac23e
Katsuke00 <katsuke00@gmail.com>
2017-06-17 19:20:45 -0300

    add ARGF() to arg.h

--- inc/util.h
+++ inc/util.h
@@ -1,6 +1,8 @@
 /* This file is part of the UtilChest from Draco Project
  * See LICENSE file for copyright and license details.
  */
+#include <sys/types.h>
+
 #include <stdio.h>
 
 #include "arg.h"
@@ -8,8 +10,11 @@
 /* fshut.c */
 int fshut(const char *, FILE *);
 
-/* put.c */
+/* putstr.c */
 void putstr(const char *, FILE *);
+
+/* strtomode.c */
+mode_t strtomode(const char *, mode_t);
 
 /* warn.c */
 int pwarn(const char *, ...);
