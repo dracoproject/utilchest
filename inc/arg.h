d8bdde5ad9e9407e54bf17983b5004c3797969a3
Katsuke00 <katsuke00@gmail.com>
2017-07-24 19:05:47 -0300

    add ifndef to arg.h

--- inc/arg.h
+++ inc/arg.h
@@ -1,6 +1,9 @@
 /* This file is part of the UtilChest from EltaninOS
  * See LICENSE file for copyright and license details.
  */
+#ifndef __ARG_H__
+#define __ARG_H__
+
 #define ARGBEGIN \
 for (argc--, argv++;\
      *argv && (*argv)[0] == '-' && (*argv)[1]; argc--, argv++) {\
@@ -24,3 +27,5 @@ for (argc--, argv++;\
 #define EARGF(x) \
 ((argv[0][1] == '\0' && !argv[1]) ? ((x), abort(), (char *)0) :\
 (_brk = 1, (argv[0][1] != '\0') ? (&argv[0][1]) : (argc--, argv++, argv[0])))
+
+#endif
