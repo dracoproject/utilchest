077ed70b4b506259516cc32987daaaa115903119
Katsuke00 <katsuke00@gmail.com>
2017-07-28 20:53:12 -0300

    use a struct to pass args in lib/fs/cp

--- lib/fs/cp.c
+++ lib/fs/cp.c
@@ -14,9 +14,16 @@
 
 #include "fs.h"
 
+struct copy {
+	const char *src;
+	const char *dest;
+	int opts;
+	struct stat st;
+};
+
 /* internal functions */
 static int
-copy_reg(const char *src, const char *dest, int opts, struct stat *st)
+copy_reg(struct copy *cp)
 {
 	char buf[BUFSIZ];
 	int sf = -1, tf = -1, rval = 0;
@@ -24,53 +31,53 @@ copy_reg(const char *src, const char *dest, int opts, struct stat *st)
 	struct stat st1;
 	struct timespec times[2];
 
-	if ((sf = open(src, O_RDONLY, 0)) < 0) {
-		warn("open %s", src);
+	if ((sf = open(cp->src, O_RDONLY, 0)) < 0) {
+		warn("open %s", cp->src);
 		return 1;
 	}
 
 	fstat(sf, &st1);
 
-	if (st->st_ino != st1.st_ino
-	    || st->st_dev != st1.st_dev) {
-		warnx("%s: changed between calls\n", src);
+	if (cp->st.st_ino != st1.st_ino ||
+	    cp->st.st_dev != st1.st_dev) {
+		warnx("%s: changed between calls\n", cp->src);
 		rval = 1;
 		goto clean;
 	}
 
-	if ((tf = open(dest, O_WRONLY|O_CREAT|O_EXCL, 0)) < 0) {
-		warn("open %s", dest);
+	if ((tf = open(cp->dest, O_WRONLY|O_CREAT|O_EXCL, 0)) < 0) {
+		warn("open %s", cp->dest);
 		rval = 1;
 		goto clean;
 	}
 
 	while ((rf = read(sf, buf, sizeof(buf))) > 0) {
 		if (write(tf, buf, rf) != rf) {
-			warn("write %s", dest);
+			warn("write %s", cp->dest);
 			rval = 1;
 			goto clean;
 		}
 	}
 
 	if (rf < 0) {
-		warn("read %s", src);
+		warn("read %s", cp->src);
 		rval = 1;
 		goto clean;
 	}
 
 	fchmod(tf, st1.st_mode);
-	if (CP_PFLAG & opts) {
+	if (CP_PFLAG & cp->opts) {
 		times[0] = st1.st_atim;
 		times[1] = st1.st_mtim;
 
-		if ((utimensat(AT_FDCWD, dest, times, 0)) < 0) {
-			warn("utimensat %s", dest);
+		if ((utimensat(AT_FDCWD, cp->dest, times, 0)) < 0) {
+			warn("utimensat %s", cp->dest);
 			rval = 1;
 			goto clean;
 		}
 
 		if ((fchown(tf, st1.st_uid, st1.st_gid)) < 0) {
-			warn("fchown %s", dest);
+			warn("fchown %s", cp->dest);
 			rval = 1;
 			goto clean;
 		}
@@ -84,30 +91,30 @@ clean:
 }
 
 static int
-copy_lnk(const char *src, const char *dest, int opts, struct stat *st)
+copy_lnk(struct copy *cp)
 {
 	char path[PATH_MAX];
 	ssize_t rl;
 
-	if ((rl = readlink(src, path, sizeof(path)-1)) < 0) {
-		warn("readlink %s", src);
+	if ((rl = readlink(cp->src, path, sizeof(path)-1)) < 0) {
+		warn("readlink %s", cp->src);
 		return 1;
 	}
 	path[rl] = '\0';
 
-	if (st->st_size < rl) {
-		warnx("%s: symlink increased in size\n", src);
+	if (cp->st.st_size < rl) {
+		warnx("%s: symlink increased in size\n", cp->src);
 		return 1;
 	}
 
-	if (symlink(path, dest) < 0) {
-		warn("symlink %s -> %s", dest, path);
+	if (symlink(path, cp->dest) < 0) {
+		warn("symlink %s -> %s", cp->dest, path);
 		return 1;
 	}
 
-	if ((CP_PFLAG & opts)
-	    && lchown(dest, st->st_uid, st->st_gid) < 0) {
-		warn("lchown %s", dest);
+	if ((CP_PFLAG & cp->opts)
+	    && lchown(cp->dest, cp->st.st_uid, cp->st.st_gid) < 0) {
+		warn("lchown %s", cp->dest);
 		return 1;
 	}
 
@@ -115,10 +122,10 @@ copy_lnk(const char *src, const char *dest, int opts, struct stat *st)
 }
 
 static int
-copy_spc(const char *src, const char *dest, int opts, struct stat *st)
+copy_spc(struct copy *cp)
 {
-	if (mknod(dest, st->st_mode, st->st_dev) < 0) {
-		warn("mknod %s", dest);
+	if (mknod(cp->dest, cp->st.st_mode, cp->st.st_dev) < 0) {
+		warn("mknod %s", cp->dest);
 		return 1;
 	}
 
@@ -130,29 +137,29 @@ int
 copy_file(const char *src, const char *dest, int opts, int depth)
 {
 	int rval = 0;
-	struct stat st;
+	struct copy cp = {.src = src, .dest = dest, .opts = opts};
 
 	if (CP_FFLAG & opts)
 		unlink(dest);
 
-	if ((FS_FOLLOW(depth) ? stat : lstat)(src, &st) < 0) {
+	if ((FS_FOLLOW(depth) ? stat : lstat)(src, &cp.st) < 0) {
 		warn("lstat %s", src);
 		return 1;
 	}
 
-	switch ((st.st_mode & S_IFMT)) {
+	switch ((cp.st.st_mode & S_IFMT)) {
 	case S_IFDIR:
 		rval = 1, errno = EISDIR;
 		warn("%s", src);
 		break;
 	case S_IFLNK:
-		rval = copy_lnk(src, dest, opts, &st);
+		rval = copy_lnk(&cp);
 		break;
 	case S_IFREG:
-		rval = copy_reg(src, dest, opts, &st);
+		rval = copy_reg(&cp);
 		break;
 	default:
-		rval = copy_spc(src, dest, opts, &st);
+		rval = copy_spc(&cp);
 		break;
 	}
 
