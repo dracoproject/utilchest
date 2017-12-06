42781bad6ffce538bd667be31b2130a1f6b8bb59
Katsuke00 <katsuke00@disroot.org>
2017-12-06 17:29:55 -0200

    fix bug, and merge tee in main
    loop searching for more args
    than given.
    
    argv = argc < fslen

--- src/tee.c
+++ src/tee.c
@@ -10,24 +10,6 @@
 #define MODE O_WRONLY|O_CREAT
 
 static void
-tee(char **name, int *fs, int len)
-{
-	char buf[BUFSIZ];
-	int i;
-	ssize_t n;
-
-	while ((n = read(STDIN_FILENO, buf, sizeof(buf))) > 0) {
-		for (i = 0; i < len; i++)
-			if (write(fs[i], buf, n) != n)
-				err(1, "write %s",
-				    (i < len) ? name[i] : "<stdout>");
-	}
-
-	if (n < 0)
-		err(1, "read <stdin>");
-}
-
-static void
 usage(void)
 {
 	fprintf(stderr, "usage: %s [-ai] [file ...]\n", getprogname());
@@ -37,8 +19,10 @@ usage(void)
 int
 main(int argc, char *argv[])
 {
+	char buf[BUFSIZ];
 	int *fs, fslen, i;
 	int aflag = O_TRUNC, iflag = 0, rval = 0;
+	ssize_t n;
 
 	setprogname(argv[0]);
 
@@ -60,11 +44,21 @@ main(int argc, char *argv[])
 	if (!(fs = malloc(fslen * sizeof(*fs))))
 		err(1, "malloc");
 
-	for (i = 0; i < fslen; i++)
+	for (i = 0; i < argc; i++)
 		if ((fs[i] = open(argv[i], MODE|aflag, DEFFILEMODE)) < 0)
 			err(1, "open %s", argv[i]);
+
 	fs[i] = STDOUT_FILENO;
-	tee(argv, fs, fslen);
+
+	while ((n = read(STDIN_FILENO, buf, sizeof(buf))) > 0) {
+		for (i = 0; i < fslen; i++)
+			if (write(fs[i], buf, n) != n)
+				err(1, "write %s",
+				    (i < fslen) ? argv[i] : "<stdout>");
+	}
+
+	if (n < 0)
+		err(1, "read <stdin>");
 
 	for (i = 0; i < fslen; i++) {
 		if (close(fs[i]) < 0) {
